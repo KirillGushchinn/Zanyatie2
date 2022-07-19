613b2ad9e8abfd701d3c9d13dcf7f6bffc9ff8b8
Kirill <krl.gushchinn@mail.ru>
2022-07-19 19:09:15 +0800

    Working version task 15

--- c_lab3/Program.cs
+++ c_lab3/Program.cs
@@ -1,15 +1,23 @@
-﻿Console.WriteLine("vvedite den nedeli: ");
+﻿Console.WriteLine("Введите день недели: ");
 int input = Convert.ToInt32(Console.ReadLine());
 
     string itog = Day(input);
     Console.WriteLine(itog);
 
-string Day (int Num) {
+string Day (int Num) 
+{
     String result;
-    if ((Num==6)||(Num==7)){
-        result="Да";
-    } else {
-         result="Нет";
+    if ((Num==6)||(Num==7))
+    {
+        result="Выходной день";
+    } 
+    else if (Num < 1 || Num > 7)
+    result="Это не день недели";
+    else 
+    {
+         result="Рабочий день";
     }
+    
+    
     return result;
 }
